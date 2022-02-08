b4b6bf82ab0bd8ffaea2356fb9299cb43d449cf2
Dean <deanscottg@gmail.com>
2022-02-08 12:19:15 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks(){
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then((books) =>renderBooks(books))
 }
+
+// function fetchBooks('https://anapioficeandfire.com/api/books')
 
 function renderBooks(books) {
   const main = document.querySelector('main');
